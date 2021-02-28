72e16e2ac4cf1fb47d19f16bccf973b352f7a7f3
daemord <daemord@gmail.com>
2021-02-28 14:53:33 +0000

    Day04

--- day04.py
+++ day04.py
@@ -1,23 +1,29 @@
 #https://adventofcode.com/2015/day/4
 
 import hashlib
-import re
+import time
+
+start_time = time.time()
 
 inputKey = "bgvyzdsv"
 
-def main(secretKey,rex):
+def main(secretKey):
     hash = secretKey
     i = 0
-    while not (re.match(rex,hashlib.md5(hash.encode()).hexdigest())):
+    answer1 = 0
+    hashCheck = hashlib.md5(hash.encode()).hexdigest()
+    while not hashCheck[:6] == '000000':
+        if answer1 == 0 and hashCheck[:5] == '00000':
+               answer1 = i
         i += 1
-        hash = secretKey + str(i)
-    return i
+        hashCheck = hashlib.md5((secretKey + str(i)).encode()).hexdigest()
+    return answer1, i
 
-answer1 = main(inputKey,"^00000")
+answer = main(inputKey)
 print("Answer 1")
-print(answer1)
-answer2 = main(inputKey,"^000000")
+print(answer[0])
 print("Answer 2")
-print(answer2)
+print(answer[1])
 
+print('Took', round(time.time() - start_time,2), 'seconds to complete')
 
